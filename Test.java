bd82dffb3a870ece3e9588ca95b87d29f8dffdc1
Taylor Sallee <tsallee@mymail.mines.edu>
2013-02-21 18:48:36 -0700

    added a new function

--- Test.java
+++ Test.java
@@ -1,11 +1,19 @@
-
 public class Test {
-
-	/**
-	 * @param args
-	 */
+	
+	public void howAwesomeAreYou(String name) {
+		System.out.println("Is " + name + " awesome?" );
+		if ( name[0] == 'T' )
+			System.out.println(name + " is awesome!");
+		else
+			System.out.println(name + " is not very awesome...");
+	}
+	
 	public static void main(String[] args) {
-	    System.out.println("hello world");	
+	    Test test = new test();
+		System.out.println("hello world");
+	    test.howAwesomeAreYou(Tim);
+	    test.howAwesomeAreYou(Taylor);
+	    test.howAwesomeAreYou(Katie);
 
 	}
 
