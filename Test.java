e333bcf851e85f051b902955177c729b21c12750
Timothy Waite <twaite@mines.edu>
2013-02-21 18:52:35 -0700

    Added randomNumber()

--- Test.java
+++ Test.java
@@ -1,12 +1,22 @@
+import java.util.Random;
+
 
 public class Test {
+	
+	public void randomNumber() {
+		final int MAXINT = 10;
+		System.out.println("Generating a random number between 1 and " + MAXINT + ": ");
+		Random rand = new Random();
+		System.out.println("You're random number is: " + rand.nextInt(MAXINT));
+	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-	    System.out.println("hello world");	
-
+	    System.out.println("hello world");
+	    Test test = new Test();
+	    test.randomNumber();
 	}
 
 }
