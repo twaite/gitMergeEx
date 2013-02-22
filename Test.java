8dfb6bc96bf8969324ea959dc4e50029a53ff0b4
Timothy Waite <twaite@mines.edu>
2013-02-21 19:03:22 -0700

    Fixed conflicts and syntax errors

--- Test.java
+++ Test.java
@@ -1,26 +1,15 @@
-<<<<<<< HEAD
-public class Test {
-	
-	public void howAwesomeAreYou(String name) {
-		System.out.println("Is " + name + " awesome?" );
-		if ( name[0] == 'T' )
-			System.out.println(name + " is awesome!");
-		else
-			System.out.println(name + " is not very awesome...");
-	}
-	
-	public static void main(String[] args) {
-	    Test test = new test();
-		System.out.println("hello world");
-	    test.howAwesomeAreYou(Tim);
-	    test.howAwesomeAreYou(Taylor);
-	    test.howAwesomeAreYou(Katie);
-
-=======
 import java.util.Random;
 
 
 public class Test {
+	
+	public void howAwesomeAreYou(String name) {
+		System.out.println("Is " + name + " awesome?" );
+		if ( name.charAt(0) == 'T' )
+			System.out.println(name + " is awesome!");
+		else
+			System.out.println(name + " is not very awesome...");
+	}
 	
 	public void randomNumber() {
 		final int MAXINT = 10;
@@ -36,7 +25,9 @@ public class Test {
 	    System.out.println("hello world");
 	    Test test = new Test();
 	    test.randomNumber();
->>>>>>> e333bcf851e85f051b902955177c729b21c12750
+	    test.howAwesomeAreYou("Tim");
+	    test.howAwesomeAreYou("Taylor");
+	    test.howAwesomeAreYou("Katie");
 	}
 
 }
